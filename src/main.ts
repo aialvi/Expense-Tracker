03ab856bb774a6e13bc266bea7cd61667e5df3a1
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-31 19:12:51 +0600

    Add Firebase and configure env

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,17 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./index.css";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount("#app");
